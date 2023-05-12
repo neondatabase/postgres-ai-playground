1faa7d9b21ae234452ae09ec679cda6ce69a3744
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-05-12 23:20:56 +0300

    update copy

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -129,7 +129,7 @@ export const Results = () => {
         <div className="flex flex-1 flex-col items-center justify-center space-y-3">
           <Icon name="Inbox" className="h-8 w-8" />
           <p className="text-center">
-            The results of your query will appear here.
+            The results of your query will appear here
           </p>
         </div>
       )}
