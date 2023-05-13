ac2b90dcb4a470b44126325c2976c28ba5ced4ba
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-05-14 00:22:06 +0300

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,6 @@ export const metadata: Metadata = {
   title: 'Postgres AI Playground',
   description:
     'Postgres playground where you can connect to your database and use AI to generate SQL queries',
-  openGraph: {
-    type: 'website',
-    url: 'https://postgres-ai-playground.vercel.app/',
-    siteName: 'Postgres AI Playground',
-    title: 'Postgres AI Playground',
-    description:
-      'Postgres playground where you can connect to your database and use AI to generate SQL queries',
-    images: ['/og-image.png'],
-  },
 };
 
 export default function RootLayout({
