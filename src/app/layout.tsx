c97e986b18cdc037fd5fc042bcaf1d2db6bf5360
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-05-14 00:15:48 +0300

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,23 @@ import Providers from '@/components/providers';
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 import { cn } from '@/utils/cn';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Postgres AI Playground',
   description:
     'Postgres playground where you can connect to your database and use AI to generate SQL queries',
-  image: '/images/og.png',
+  openGraph: {
+    type: 'website',
+    url: 'https://postgres-ai-playground.vercel.app/',
+    siteName: 'Postgres AI Playground',
+    title: 'Postgres AI Playground',
+    description:
+      'Postgres playground where you can connect to your database and use AI to generate SQL queries',
+    images: ['/og-image.png'],
+  },
 };
 
 export default function RootLayout({
