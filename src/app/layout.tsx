5d2dfd33fe2a271222a509c361c7dafe980d40fe
Mahmoud Abdelwahab <hey@mahmoud.codes>
2023-05-14 16:01:51 +0300

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,26 @@ export const metadata: Metadata = {
   title: 'Postgres AI Playground',
   description:
     'Postgres playground where you can connect to your database and use AI to generate SQL queries',
+  keywords: [
+    'Postgres',
+    'SQL Playground',
+    'Postgres AI',
+    'Postgres Playground',
+    'Postgres AI Playground',
+    'Postgres SQL Playground',
+    'Postgres SQL AI Playground',
+  ],
+  openGraph: {
+    images: ['/images/og.png'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Postgres AI Playground',
+    description:
+      'Postgres playground where you can connect to your database and use AI to generate SQL queries',
+    images: ['/images/og.png'],
+    creator: '@neondatabase',
+  },
 };
 
 export default function RootLayout({
